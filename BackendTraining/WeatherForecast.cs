2c95ab01f444237a75aabddf1b128ad9b15c1f39
Amine El-Mohamadi <466497@student.fontys.nl>
2024-03-04 18:13:24 +0100

    test workflow

--- BackendTraining/WeatherForecast.cs
+++ BackendTraining/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace BackendTraining
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        publisdfdc int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
